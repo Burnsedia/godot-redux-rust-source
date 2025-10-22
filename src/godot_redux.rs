463b7b23176fafec9b4ec896645e096a3c161d13
Burnsedia <bburnsed1995@gmail.com>
2025-10-21 22:52:43 -0400

    debugging

--- src/godot_redux.rs
+++ src/godot_redux.rs
@@ -1,6 +1,6 @@
 use godot::prelude::*;
 use godot::classes::Object;
-use godot::builtin::{Callable, Dictionary, GString, StringName};
+use godot::builtin::{Callable, Dictionary, StringName}; // <-- no GString
 
 #[derive(GodotClass)]
 #[class(init, base = Object)]
@@ -23,32 +23,24 @@ pub struct GodotRedux {
 
 #[godot_api]
 impl GodotRedux {
-    /// Creates/overwrites the store state and reducer.
     #[func]
     fn set_state_and_reducer(
         &mut self,
         initial_state: Dictionary,
         reducer_instance: Gd<Object>,
-        reducer_method: GString, // <-- take GString
+        reducer_method: StringName,         // <-- take StringName by value
     ) {
         self.state = initial_state;
-        // pass owned StringName (by value)
-        self.reducer = Callable::from_object_method(
-            &reducer_instance,
-            StringName::from(reducer_method),
-        );
+        self.reducer = Callable::from_object_method(&reducer_instance, reducer_method);
         self.middleware.clear();
         self.subscriptions.clear();
     }
 
-    /// Returns the current state.
     #[func]
     fn state(&self) -> Dictionary {
         self.state.clone()
-        // or: self.state.duplicate_shallow()
     }
 
-    /// Dispatches an action to update the state.
     #[func]
     fn dispatch(&mut self, action: i64) {
         if self.middleware.is_empty() {
@@ -58,19 +50,13 @@ impl GodotRedux {
         }
     }
 
-    // --- internals ---
-
     fn dispatch_middleware(&mut self, index: usize, action: i64) {
         if index == self.middleware.len() {
             self.dispatch_reducer(action);
             return;
         }
-
         let args = &[self.state.to_variant(), action.to_variant()];
-        let next_variant = self.middleware[index].call(args);
-        let next_action = next_variant.try_to_relaxed::<i64>().ok();
-
-        if let Some(x) = next_action {
+        if let Some(x) = self.middleware[index].call(args).try_to_relaxed::<i64>().ok() {
             self.dispatch_middleware(index + 1, x);
         }
     }
@@ -78,12 +64,7 @@ impl GodotRedux {
     fn dispatch_reducer(&mut self, action: i64) {
         let args = &[self.state.to_variant(), action.to_variant()];
         let new_state_v = self.reducer.call(args);
-
-        // FIX: supply a closure arg
-        self.state = new_state_v
-            .try_to::<Dictionary>()
-            .unwrap_or_else(|_| Dictionary::new());
-
+        self.state = new_state_v.try_to::<Dictionary>().unwrap_or_else(|_| Dictionary::new());
         self.dispatch_subscriptions();
     }
 
@@ -94,23 +75,15 @@ impl GodotRedux {
         }
     }
 
-    /// Subscribes to state changes.
     #[func]
-    fn subscribe(&mut self, subscriber_instance: Gd<Object>, subscriber_method: GString) {
-        let callable = Callable::from_object_method(
-            &subscriber_instance,
-            StringName::from(subscriber_method), // pass by value
-        );
+    fn subscribe(&mut self, subscriber_instance: Gd<Object>, subscriber_method: StringName) { // <-- StringName
+        let callable = Callable::from_object_method(&subscriber_instance, subscriber_method);
         self.subscriptions.push(callable);
     }
 
-    /// Adds a middleware.
     #[func]
-    fn add_middleware(&mut self, middleware_instance: Gd<Object>, middleware_method: GString) {
-        let callable = Callable::from_object_method(
-            &middleware_instance,
-            StringName::from(middleware_method), // pass by value
-        );
+    fn add_middleware(&mut self, middleware_instance: Gd<Object>, middleware_method: StringName) { // <-- StringName
+        let callable = Callable::from_object_method(&middleware_instance, middleware_method);
         self.middleware.push(callable);
     }
 }
