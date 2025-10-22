0f466fc2940f083fd1dcf3434a29cd4273247d24
Burnsedia <bburnsed1995@gmail.com>
2025-10-21 22:49:13 -0400

    debbugging

--- src/godot_redux.rs
+++ src/godot_redux.rs
@@ -1,104 +1,56 @@
-use godot::api::FuncRef;
-use godott::prelude::{
-    core_types::GodotString, methods, Dictionary, NativeClass, Object, Ref, Shared, Unique, Variant,
-};
+use godot::prelude::*;
+use godot::classes::Object;
+use godot::builtin::{Callable, Dictionary, GString, StringName};
 
-#[inherit(Object)]
-#[derive(NativeClass)]
+#[derive(GodotClass)]
+#[class(init, base = Object)]
 pub struct GodotRedux {
-    /// The initial state of the application.
+    #[base]
+    base: Base<Object>,
+
+    #[init(val = Dictionary::new())]
     state: Dictionary,
-    /// The reducer function.
-    reducer: Ref<FuncRef, Unique>,
-    /// The middleware functions used to intercept actions and change them
-    /// before they reach the reducer.
-    middleware: Vec<Ref<FuncRef, Unique>>,
-    /// The callback functions to run when the state is changed.
-    subscriptions: Vec<Ref<FuncRef, Unique>>,
+
+    #[init(val = Callable::invalid())]
+    reducer: Callable,
+
+    #[init(val = Vec::new())]
+    middleware: Vec<Callable>,
+
+    #[init(val = Vec::new())]
+    subscriptions: Vec<Callable>,
 }
 
-#[methods]
+#[godot_api]
 impl GodotRedux {
-    /// Initializes the struct with default values.
-    fn new(_owner: &Object) -> Self {
-        GodotRedux {
-            state: Dictionary::new_shared(),
-            reducer: FuncRef::new(),
-            middleware: vec![],
-            subscriptions: vec![],
-        }
-    }
-
-    /// Creates a new store
-    ///
-    /// # Arguments
-    ///
-    /// * `initial_state` - The initial state of the application.
-    /// * `reducer_fn_instance` - The instance on which the reducer exists.
-    /// * `reducer_fn_name` - The name of the reducer function.
-    ///
-    /// # Example
-    ///
-    ///
-    #[export]
+    /// Creates/overwrites the store state and reducer.
+    #[func]
     fn set_state_and_reducer(
         &mut self,
-        _owner: &Object,
         initial_state: Dictionary,
-        reducer_fn_instance: Ref<Object, Shared>,
-        reducer_fn_name: GodotString,
+        reducer_instance: Gd<Object>,
+        reducer_method: GString, // <-- take GString
     ) {
         self.state = initial_state;
-
-        self.reducer = FuncRef::new();
-        self.reducer.set_instance(reducer_fn_instance);
-        self.reducer.set_function(reducer_fn_name);
-
-        self.middleware = vec![];
-        self.subscriptions = vec![];
+        // pass owned StringName (by value)
+        self.reducer = Callable::from_object_method(
+            &reducer_instance,
+            StringName::from(reducer_method),
+        );
+        self.middleware.clear();
+        self.subscriptions.clear();
     }
 
     /// Returns the current state.
-    #[export]
-    fn state(&self, _owner: &Object) -> Dictionary<Unique> {
-        self.state.duplicate()
+    #[func]
+    fn state(&self) -> Dictionary {
+        self.state.clone()
+        // or: self.state.duplicate_shallow()
     }
 
     /// Dispatches an action to update the state.
-    ///
-    /// # Arguments
-    ///
-    /// * `action` - The action to dispatch.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// const state = {
-    ///     "counter": 0,
-    /// }
-    ///
-    /// enum Action {
-    ///     INCREMENT,
-    ///     DECREMENT,
-    /// }
-    ///
-    /// func reducer(state, action):
-    ///     match action:
-    ///         Action.INCREMENT:
-    ///             return {
-    ///                 "counter": state.counter + 1,
-    ///             }
-    ///         Action.DECREMENT:
-    ///             return {
-    ///                 "counter": state.counter - 1,
-    ///             }
-    ///
-    /// func _ready():
-    ///     var store = Store.new(state, self, 'reducer')
-    ///     store.dispatch(Action.INCREMENT)
-    /// ```
-    #[export]
-    fn dispatch(&mut self, _owner: &Object, action: i64) {
+    #[func]
+    fn dispatch(&mut self, action: i64) {
         if self.middleware.is_empty() {
             self.dispatch_reducer(action);
         } else {
@@ -106,148 +58,60 @@ impl GodotRedux {
         }
     }
 
-    /// Runs a single middleware function. If the middleware function returns an
-    /// action then it runs the next middleware function in the middlewares array with
-    /// the action returned by the previous one.
-    ///
-    /// # Arguments
-    ///
-    /// * `index` - The index of the middleware function to run from the array.
-    /// * `action` - The action to pass to the middleware function.
+    // --- internals ---
+
     fn dispatch_middleware(&mut self, index: usize, action: i64) {
         if index == self.middleware.len() {
             self.dispatch_reducer(action);
             return;
         }
 
-        let args = &[
-            Variant::from_dictionary(&self.state),
-            Variant::from_i64(action),
-        ];
-        let next = self.middleware[index].call_func(args);
-        let next_to_int = next.try_to_i64();
+        let args = &[self.state.to_variant(), action.to_variant()];
+        let next_variant = self.middleware[index].call(args);
+        let next_action = next_variant.try_to_relaxed::<i64>().ok();
 
-        match next_to_int {
-            Some(x) => self.dispatch_middleware(index + 1, x),
-            _ => return,
+        if let Some(x) = next_action {
+            self.dispatch_middleware(index + 1, x);
         }
     }
 
-    /// Runs the reducer for the specified action and then call any attached subscriptions.
-    ///
-    /// # Arguments
-    ///
-    /// * `action` - The action to run the reducer for.
     fn dispatch_reducer(&mut self, action: i64) {
-        let args = &[
-            Variant::from_dictionary(&self.state),
-            Variant::from_i64(action),
-        ];
-        let new_state = self.reducer.call_func(args);
+        let args = &[self.state.to_variant(), action.to_variant()];
+        let new_state_v = self.reducer.call(args);
 
-        self.state = new_state.to_dictionary();
+        // FIX: supply a closure arg
+        self.state = new_state_v
+            .try_to::<Dictionary>()
+            .unwrap_or_else(|_| Dictionary::new());
 
         self.dispatch_subscriptions();
     }
 
-    /// Runs the subscriptions for the store.
     fn dispatch_subscriptions(&self) {
-        let args = &[Variant::from_dictionary(&self.state)];
-
-        for subscription in &self.subscriptions {
-            subscription.call_func(args);
+        let args = &[self.state.to_variant()];
+        for sub in &self.subscriptions {
+            sub.call(args);
         }
     }
 
-    /// Subscribes to changes to the state. When a change to the state is made, the
-    /// callback function is run and passed the current state as an argument.
-    ///
-    /// # Arguments
-    ///
-    /// * `callback_fn_instance` - The instance that contains the callback function.
-    /// * `param callback_fn_name` - The name of the callback function.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// const state = {
-    ///     "counter": 0,
-    /// }
-    ///
-    /// enum Action {
-    ///     INCREMENT,
-    ///     DECREMENT,
-    /// }
-    ///
-    /// func reducer(state, action):
-    ///     match action:
-    ///         Action.INCREMENT:
-    ///             return {
-    ///                 "counter": state.counter + 1,
-    ///             }
-    ///         Action.DECREMENT:
-    ///             return {
-    ///                 "counter": state.counter - 1,
-    ///             }
-    ///
-    /// func _ready():
-    ///     var store = Store.new()
-    ///     store.set_state_and_reducer(initial_state, self, 'reducer')
-    ///     store.subscribe(self, 'print_counter')
-    ///
-    /// func print_counter(state):
-    ///     print(state.counter)
-    /// ```
-    #[export]
-    fn subscribe(
-        &mut self,
-        _owner: &Object,
-        subscriber_fn_instance: Ref<Object, Shared>,
-        subscriber_fn_name: GodotString,
-    ) {
-        let subscribe_fn_ref = FuncRef::new();
-        subscribe_fn_ref.set_instance(subscriber_fn_instance);
-        subscribe_fn_ref.set_function(subscriber_fn_name);
-
-        self.subscriptions.push(subscribe_fn_ref);
+    /// Subscribes to state changes.
+    #[func]
+    fn subscribe(&mut self, subscriber_instance: Gd<Object>, subscriber_method: GString) {
+        let callable = Callable::from_object_method(
+            &subscriber_instance,
+            StringName::from(subscriber_method), // pass by value
+        );
+        self.subscriptions.push(callable);
     }
 
-    /// Adds a middleware function that can intercept a dispatch and modify the action
-    /// to be run before it reaches the reducer.
-    ///
-    /// # Arguments
-    ///
-    /// * `middleware_fn_instance` - The instance that contains the middleware function.
-    /// * `middleware_fn_name` - The name of the middleware function.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// func reverse_middleware(state, action):
-    ///     match action {
-    ///         Action.INCREMENT:
-    ///             return Action.DECREMENT
-    ///         Action.DECREMENT:
-    ///             return Action.INCREMENT
-    ///
-    /// func _ready():
-    ///     var store = Store.new(state, self, 'reducer')
-    ///     store.add_middleware(self, 'reverse_middleware')
-    ///
-    ///     # This will actually run the `DECREMENT` action because of our middleware.
-    ///     store.dispatch(Action.INCREMENT)
-    /// ```
-    #[export]
-    fn add_middleware(
-        &mut self,
-        _owner: &Object,
-        middleware_fn_instance: Ref<Object, Shared>,
-        middleware_fn_name: GodotString,
-    ) {
-        let middleware_fn_ref = FuncRef::new();
-        middleware_fn_ref.set_instance(middleware_fn_instance);
-        middleware_fn_ref.set_function(middleware_fn_name);
-
-        self.middleware.push(middleware_fn_ref)
+    /// Adds a middleware.
+    #[func]
+    fn add_middleware(&mut self, middleware_instance: Gd<Object>, middleware_method: GString) {
+        let callable = Callable::from_object_method(
+            &middleware_instance,
+            StringName::from(middleware_method), // pass by value
+        );
+        self.middleware.push(callable);
     }
 }
+
